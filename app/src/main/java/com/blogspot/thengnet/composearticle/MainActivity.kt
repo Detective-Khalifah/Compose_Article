b12123dba7d6a5c6958f511773d937b301e01217
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2023-06-03 18:44:31 +0100

    Add heading, sub-heading, and content images & Image in a Composable Column

--- app/src/main/java/com/blogspot/thengnet/composearticle/MainActivity.kt
+++ app/src/main/java/com/blogspot/thengnet/composearticle/MainActivity.kt
@@ -3,13 +3,21 @@ package com.blogspot.thengnet.composearticle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.blogspot.thengnet.composearticle.ui.theme.ComposeArticleTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Article()
                 }
             }
         }
@@ -30,17 +38,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Article(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Image(
+            painter = painterResource(id = R.drawable.bg_compose_background),
+            contentDescription = null
+        )
+        Text(
+            text = stringResource(R.string.jetpack_compose_tutorial_heading),
+            fontSize = 24.sp,
+            modifier = modifier.padding(16.dp),
+        )
+        Text(
+            text = stringResource(R.string.jetpack_compose_tutorial_sub_heading),
+            textAlign = TextAlign.Justify,
+            modifier = modifier.padding(start = 16.dp, end = 16.dp),
+        )
+        Text(
+            text = stringResource(R.string.jetpack_compose_tutorial_content),
+            textAlign = TextAlign.Justify,
+            modifier = modifier.padding(16.dp),
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeArticleTheme {
-        Greeting("Android")
+        Article()
     }
 }
